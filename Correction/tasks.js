8cca8d60d679e2abe00da570d44601940c86b171
Richard Szigehty <91049248+PreVizsla0410@users.noreply.github.com>
2022-02-08 14:13:50 +0100

    Add files via upload

---
+++ Correction/tasks.js
@@ -0,0 +1,74 @@
+let array = `[{
+   "id":"1",
+    "task": "make breakfast",
+    "description": "high fat or proteine breakfast, to be strong",
+    "img": "https://images.eatsmarter.de/sites/default/files/styles/max_size/public/english-breakfast-mit-sojawuerstchen-666085.jpg",
+    "Priority_level": "0",
+    "Deadline": "5.02.2022"
+ }, 
+ {
+   "id":"2",
+     "task": "sports",
+     "description": "to activate strenght",
+     "img": "https://image.stern.de/31597632/t/Wn/v2/w960/r1.7778/-/olympia-2022--deutscher-fehlstart---so-lief-der-eroeffnungstag.jpg",
+     "Priority_level": "0",
+     "Deadline": "5.02.2022"
+  }, 
+  {
+    "id":"3",
+     "task": "Go with the dog",
+     "description": "you just cannot skip that",
+     "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZHVZnyIldTLBqhYY6PfQnAgGeNBluXRpOCn61T4nDwU28gCN0wRje6C9BkETbGf-lEqY&usqp=CAU",
+     "Priority_level": "0",
+     "Deadline": "5.02.2022"
+  }, 
+  {
+    "id":"4",
+     "task": "learn",
+     "description": "Try to grow your tiny brain at least a little bit",
+     "img": "2wCEAAkGBxEQEBUTEhIRFRUXGRcSExUVDQ8QGBAVHhMYGRUaFRMYHSggGRolHhYVIzEhJSkrLi4uGCAzODMsNygtLisBCgoKDg0OGxAQGi0lHyYtLTMtLS8tLSstLS0vLS0tLS0wLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0vLi0tLS0tLf",
+     "Priority_level": "0",
+     "Deadline": "5.02.2022"
+  }, 
+  {
+    "id":"5",
+     "task": "Lunch",
+     "description": "eat",
+     "img": "https://p6.focus.de/img/fotos/id_7544063/worstofchefkoch1.jpg?im=Crop%3D%280%2C81%2C960%2C480%29%3BResize%3D%281200%2C627%29&impolicy=perceptual&quality=medium&hash=9c7d88f8181e6302044f25337728575427199dca999cb93b2ede0c5c5e02e923",
+     "Priority_level": "0",
+     "Deadline": "5.02.2022"
+  }, 
+  {
+    "id":"6",
+     "task": "work",
+     "description": "you have to do it",
+     "img": "https://de.web.img3.acsta.net/r_654_368/newsv7/22/01/19/11/26/2415276.jpg",
+     "Priority_level": "0",
+     "Deadline": "5.02.2022"
+  }, 
+  {
+    "id":"7",
+     "task": "feed the dog",
+     "description": "feed the poor thing",
+     "img": "https://www.aspca.org/sites/default/files/apcc_people-foods-to-avoid_main-image.jpg",
+     "Priority_level": "0",
+     "Deadline": "5.02.2022"
+  }, 
+  {
+    "id":"8",
+     "task": "fix the car",
+     "description": "not again, but yes",
+     "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7_HmbtWXGYDD_kv2_GolBQ9lbR1qGNfOmbg&usqp=CAU",
+     "Priority_level": "0",
+     "Deadline": "5.02.2022"
+  },
+  {
+    "id":"9",
+     "task": "visit family",
+     "description": "They make your life so much brighter",
+     "img": "https://www.cnet.com/a/img/tYzDU83vcPvOXIjtzr9KMWDbEDs=/0x120:1048x709/940x0/2020/01/31/bd64bb36-7a46-47e7-a847-166bb244e320/vin-diesel-f9.jpg",
+     "Priority_level": "0",
+     "Deadline": "5.02.2022"
+  }]`
+ ;
+ 
